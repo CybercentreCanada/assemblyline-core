ab70d9d1aa5a8484267b823c30af4c6cb4f7d275
Adam Douglass <douglass.cse@gmail.com>
2019-02-22 16:56:30 -0500

    Tidy up the middleman tests.

--- al_core/middleman/test_worker_ingest.py
+++ al_core/middleman/test_worker_ingest.py
@@ -1,12 +1,12 @@
-import logging
+
 import json
+import pytest
 from unittest import mock
 import time
 
-from assemblyline.common import log
 
 from al_core.middleman.run_ingest import MiddlemanIngester
-from al_core.middleman.middleman import Middleman, IngestTask
+from al_core.middleman.middleman import IngestTask
 from .client import MiddlemanClient
 
 
@@ -16,6 +16,7 @@ from assemblyline.datastore.helper import AssemblylineDatastore
 from al_core.mocking import clean_redis
 
 class TrueCountTimes:
+    """A helper object that replaces a boolean, switching from true to false after its value is read."""
     def __init__(self, count):
         self.counter = count
 
@@ -24,52 +25,8 @@ class TrueCountTimes:
         return self.counter >= 0
 
 
-def rgetattr(obj, path):
-    if isinstance(path, str):
-        path = path.split('.')
-    if len(path) == 1:
-        return getattr(obj, path[0])
-    return rgetattr(getattr(obj, path[0]), path[1:])
-
-
-class MakeMiddleman:
-    def __init__(self):
-        self.instance = None
-        self.calls = []
-        self.client_calls = []
-        self.assignments = []
-        self.client = MiddlemanClient()
-
-    def __call__(self, *args, **kwargs):
-        if not self.instance:
-            self.instance = Middleman(*args, **kwargs)
-
-            for call in self.calls:
-                method = rgetattr(self.instance, call[0])
-                method(*call[1], **call[2])
-
-            for call in self.assignments:
-                setattr(self.instance, call[0], call[1])
-
-            for method, args, kwargs in self.client_calls:
-                method = rgetattr(self.client, method)
-                print(len(args))
-                print(len(kwargs))
-                method(*args, **kwargs)
-
-        return self.instance
-
-    def call(self, method, *args, **kwargs):
-        self.calls.append((method, args, kwargs))
-
-    def client_call(self, method, *args, **kwargs):
-        self.client_calls.append((method, args, kwargs))
-
-    def assign(self, attribute_name, value):
-        self.assignments.append((attribute_name, value))
-
-
 def make_message(**message):
+    """A helper function to fill in some fields that are largely invariant across tests."""
     send = dict(
         # describe the file being ingested
         sha256='0'*64,
@@ -87,14 +44,31 @@ def make_message(**message):
     return send
 
 
+@pytest.fixture
 @mock.patch('al_core.middleman.middleman.SubmissionTool', new=mock.MagicMock())
-def test_ingest_simple(clean_redis):
-    mi = MiddlemanIngester(datastore=AssemblylineDatastore(MockDatastore()), redis=clean_redis, persistent_redis=clean_redis)
+def ingest_harness(clean_redis):
+    """"Setup a test environment.
 
-    mi.running = TrueCountTimes(2)
-
+    By using a fake redis and datastore, we:
+        a) ensure that this test runs regardless of any code errors in replaced modules
+        b) ensure that the datastore and redis is EMPTY every time the test runs
+           isolating this test from any other test run at the same time
+    """
+    datastore = AssemblylineDatastore(MockDatastore())
+    ingester = MiddlemanIngester(datastore=datastore, redis=clean_redis, persistent_redis=clean_redis)
     client = MiddlemanClient(redis=clean_redis, persistent_redis=clean_redis)
+    ingester.running = TrueCountTimes(1)
+    return datastore, ingester, client
+
+def test_ingest_simple(ingest_harness):
+    datastore, ingester, client = ingest_harness
+    # Let the ingest loop run an extra time because we send two messages
+    ingester.running.counter += 1
+
+    # Send a message with a garbled sha, this should be dropped
     client.ingest(**make_message(sha256='1'*10))
+
+    # Send a message that is fine, but has an illegal metadata field
     client.ingest(**make_message(
         metadata={
             'tobig': 'a' * (client.config.submission.max_metadata_length + 2),
@@ -102,39 +76,39 @@ def test_ingest_simple(clean_redis):
         }
     ))
 
-    mi.try_run()
+    # Process those messages
+    ingester.try_run()
 
-    mm = mi.middleman
+    mm = ingester.middleman
     # The only task that makes it through though fit these parameters
     task = mm.unique_queue.pop()
     assert task
     task = IngestTask(json.loads(task))
-    assert task.sha256 == '0' * 64
-    assert 'tobig' not in task.metadata
-    assert task.metadata['small'] == '100'
+    assert task.sha256 == '0' * 64  # Only the valid sha passed through
+    assert 'tobig' not in task.metadata  # The bad metadata was stripped
+    assert task.metadata['small'] == '100'  # The valid metadata is unchanged
 
     # None of the other tasks should reach the end
     assert mm.unique_queue.length() == 0
     assert mm.ingest_queue.length() == 0
 
 
-@mock.patch('al_core.middleman.middleman.SubmissionTool', new=mock.MagicMock())
-def test_ingest_stale_score_exists(clean_redis):
+def test_ingest_stale_score_exists(ingest_harness):
+    datastore, ingester, client = ingest_harness
+
+    # Add a stale file score to the database for every file always
     from assemblyline.odm.models.filescore import FileScore
-    ds = AssemblylineDatastore(MockDatastore())
-    ds.filescore.get = mock.MagicMock(return_value=FileScore(dict(psid='000', expiry_ts=0, errors=0, score=10, sid='000', time=0)))
+    datastore.filescore.get = mock.MagicMock(return_value=FileScore(dict(psid='000', expiry_ts=0, errors=0, score=10, sid='000', time=0)))
 
-    mi = MiddlemanIngester(datastore=ds, redis=clean_redis, persistent_redis=clean_redis)
-
-    mi.running = TrueCountTimes(1)
-
-    client = MiddlemanClient(redis=clean_redis, persistent_redis=clean_redis)
+    # Process a message that hits the stale score
     client.ingest(**make_message())
+    ingester.try_run()
 
-    mi.try_run()
+    # The stale filescore was retrieved
+    datastore.filescore.get.assert_called_once()
 
-    mm = mi.middleman
-
+    # but message was ingested as a cache miss
+    mm = ingester.middleman
     task = mm.unique_queue.pop()
     assert task
     task = IngestTask(json.loads(task))
@@ -144,58 +118,47 @@ def test_ingest_stale_score_exists(clean_redis):
     assert mm.ingest_queue.length() == 0
 
 
-@mock.patch('al_core.middleman.middleman.SubmissionTool', new=mock.MagicMock())
-def test_ingest_score_exists(clean_redis):
+def test_ingest_score_exists(ingest_harness):
+    datastore, ingester, client = ingest_harness
+
+    # Add a valid file score for all files
     from assemblyline.odm.models.filescore import FileScore
-    ds = AssemblylineDatastore(MockDatastore())
-    ds.filescore.get = mock.MagicMock(return_value=FileScore(dict(psid='000', expiry_ts=0, errors=0, score=10, sid='000', time=time.time())))
+    datastore.filescore.get = mock.MagicMock(return_value=FileScore(dict(psid='000', expiry_ts=0, errors=0, score=10, sid='000', time=time.time())))
 
-    mi = MiddlemanIngester(datastore=ds, redis=clean_redis, persistent_redis=clean_redis)
-    mi.running = TrueCountTimes(1)
-    MiddlemanClient(redis=clean_redis, persistent_redis=clean_redis).ingest(**make_message())
-    mi.try_run()
+    # Ingest a file
+    client.ingest(**make_message())
+    ingester.try_run()
 
-    assert mi.middleman.unique_queue.length() == 0
-    assert mi.middleman.ingest_queue.length() == 0
+    # No file has made it into the internal buffer => cache hit and drop
+    datastore.filescore.get.assert_called_once()
+    assert ingester.middleman.unique_queue.length() == 0
+    assert ingester.middleman.ingest_queue.length() == 0
 
 
-@mock.patch('al_core.middleman.middleman.SubmissionTool', new=mock.MagicMock())
-def test_ingest_groups_error():
-    ds = AssemblylineDatastore(MockDatastore())
-    middleman_factory = MakeMiddleman()
-    middleman_factory.assign("running", TrueCountTimes(1))
-    middleman_factory.client_call('ingest', **make_message(
-        params={'groups': []},
-    ))
+def test_ingest_groups_error(ingest_harness):
+    datastore, ingester, client = ingest_harness
 
-    log.init_logging("middleman")
-    logger = logging.getLogger('assemblyline.middleman.ingester')
+    # Send a message with invalid group parameter, and user data missing
+    client.ingest(**make_message(params={'groups': []}))
+    ingester.try_run()
 
-    with mock.patch('middleman.run_ingest.Middleman', middleman_factory):
-        ingester(logger=logger, datastore=ds)
+    # dropped file with no known user
+    assert ingester.middleman.unique_queue.length() == 0
+    assert ingester.middleman.ingest_queue.length() == 0
 
-    mm = middleman_factory.instance
+
+def test_ingest_size_error(ingest_harness):
+    datastore, ingester, client = ingest_harness
+
+    # Send a rather big file
+    client.ingest(**make_message(file_size=10**10))
+    ingester.try_run()
+
+    # No files in the internal buffer
+    mm = ingester.middleman
     assert mm.unique_queue.length() == 0
     assert mm.ingest_queue.length() == 0
 
-
-@mock.patch('al_core.middleman.middleman.SubmissionTool', new=mock.MagicMock())
-def test_ingest_size_error():
-    ds = AssemblylineDatastore(MockDatastore())
-    middleman_factory = MakeMiddleman()
-    middleman_factory.assign("running", TrueCountTimes(1))
-    middleman_factory.client_call('ingest', **make_message(
-        file_size=10**10,
-    ))
-
-    log.init_logging("middleman")
-    logger = logging.getLogger('assemblyline.middleman.ingester')
-
-    with mock.patch('middleman.run_ingest.Middleman', middleman_factory):
-        ingester(logger=logger, datastore=ds)
-
-    mm = middleman_factory.instance
-    assert mm.unique_queue.length() == 0
-    assert mm.ingest_queue.length() == 0
-    assert mm.drop_queue.pop() is not None
+    # A file was dropped
+    assert mm.drop_queue.pop(blocking=False) is not None
     assert mm.drop_queue.length() == 0
--- al_core/middleman/test_worker_submit.py
+++ al_core/middleman/test_worker_submit.py
@@ -1,30 +1,35 @@
-import logging
 from unittest import mock
-import fakeredis
 import pytest
 import time
 
 
-from assemblyline.common import log
 from assemblyline.odm.models.submission import SubmissionParams
 from assemblyline.odm.models.filescore import FileScore
 
-from .test_worker_ingest import AssemblylineDatastore, MockDatastore, MakeMiddleman, TrueCountTimes
-import al_core.middleman.run_submit
+from al_core.middleman.client import MiddlemanClient
+from .test_worker_ingest import AssemblylineDatastore, MockDatastore, TrueCountTimes
+from al_core.middleman.run_submit import MiddlemanSubmitter
 from al_core.middleman.middleman import IngestTask, _dup_prefix
+
+from al_core.mocking import clean_redis
 
 
 @pytest.fixture
-def clean_redis():
-    return fakeredis.FakeStrictRedis()
+@mock.patch('al_core.middleman.middleman.SubmissionTool', new=mock.MagicMock())
+def submit_harness(clean_redis):
+    """Setup a test environment just file for the ingest tests"""
+    datastore = AssemblylineDatastore(MockDatastore())
+    submitter = MiddlemanSubmitter(datastore=datastore, redis=clean_redis, persistent_redis=clean_redis)
+    client = MiddlemanClient(redis=clean_redis, persistent_redis=clean_redis)
+    submitter.running = TrueCountTimes(1)
+    return datastore, submitter, client
 
 
-@mock.patch('al_core.middleman.middleman.SubmissionTool', mock.MagicMock())
-def test_submit_simple(clean_redis):
-    ds = AssemblylineDatastore(MockDatastore())
-    mf = MakeMiddleman()
-    mf.assign('running', TrueCountTimes(1))
-    mf.call('unique_queue.push', 0, IngestTask({
+def test_submit_simple(submit_harness):
+    datastore, submitter, client = submit_harness
+
+    # Push a normal ingest task
+    submitter.middleman.unique_queue.push(0, IngestTask({
         'params': SubmissionParams({
             'classification': 'U',
             'services': {
@@ -40,26 +45,18 @@ def test_submit_simple(clean_redis):
         'classification': 'U',
         'metadata': {}
     }).json())
+    submitter.try_run(volatile=True)
 
-    log.init_logging("middleman")
-    logger = logging.getLogger('assemblyline.middleman.ingester')
-
-    with mock.patch('al_core.middleman.run_submit.Middleman', mf):
-        al_core.middleman.run_submit.submitter(logger=logger, datastore=ds, volatile=True,
-                                               redis=clean_redis, persistent_redis=clean_redis)
-
-    # No tasks should be left in the queue
-    mm = mf.instance
+    # The task has been passed to the submit tool and there are no other submissions
+    mm = submitter.middleman
+    mm.submit_tool.submit.assert_called()
     assert mm.unique_queue.pop() is None
-    mm.client.submit.assert_called()
 
 
-@mock.patch('al_core.middleman.middleman.SubmissionTool', mock.MagicMock())
-def test_submit_duplicate(clean_redis):
-    ds = AssemblylineDatastore(MockDatastore())
-    mf = MakeMiddleman()
-    mf.assign('running', TrueCountTimes(1))
-    mf.assign('client', mock.MagicMock())
+def test_submit_duplicate(submit_harness):
+    datastore, submitter, client = submit_harness
+
+    # a normal ingest task
     task = IngestTask({
         'params': SubmissionParams({
             'classification': 'U',
@@ -76,33 +73,30 @@ def test_submit_duplicate(clean_redis):
         'classification': 'U',
         'metadata': {}
     })
+    # Make sure the scan key is correct, this is normally done on ingest
     task.scan_key = task.params.create_filescore_key(task.sha256, [])
-    mf.call('scanning.add', task.scan_key, task.as_primitives())
-    mf.call('unique_queue.push', 0, task.json())
 
-    log.init_logging("middleman")
-    logger = logging.getLogger('assemblyline.middleman.ingester')
+    # Add this file to the scanning table, so it looks like it has already been submitted + ingest again
+    submitter.middleman.scanning.add(task.scan_key, task.as_primitives())
+    submitter.middleman.unique_queue.push(0, task.json())
 
-    with mock.patch('al_core.middleman.run_submit.Middleman', mf):
-        al_core.middleman.run_submit.submitter(logger=logger, datastore=ds, volatile=True,
-                                               redis=clean_redis, persistent_redis=clean_redis)
+    submitter.try_run(volatile=True)
 
     # No tasks should be left in the queue
-    mm = mf.instance
-    # No tasks should be left in the queue
+    mm = submitter.middleman
     assert mm.unique_queue.pop() is None
-    # The task should have  been pushed to the duplicates queue
+    # The task should have been pushed to the duplicates queue
     assert mm.duplicate_queue.length(_dup_prefix + task.scan_key) == 1
 
 
-@mock.patch('al_core.middleman.middleman.SubmissionTool', mock.MagicMock())
-def test_existing_score(clean_redis):
-    ds = AssemblylineDatastore(MockDatastore())
-    ds.filescore.get = mock.MagicMock(return_value=FileScore(dict(psid='000', expiry_ts=0, errors=0, score=10, sid='000', time=time.time())))
-    mf = MakeMiddleman()
-    mf.assign('running', TrueCountTimes(1))
-    mf.assign('client', mock.MagicMock())
-    mf.call('unique_queue.push', 0, IngestTask({
+def test_existing_score(submit_harness):
+    datastore, submitter, client = submit_harness
+
+    # Set everything to have an existing filestore
+    datastore.filescore.get = mock.MagicMock(return_value=FileScore(dict(psid='000', expiry_ts=0, errors=0, score=10, sid='000', time=time.time())))
+
+    # add task to internal queue
+    submitter.middleman.unique_queue.push(0, IngestTask({
         'params': SubmissionParams({
             'classification': 'U',
             'services': {
@@ -120,16 +114,11 @@ def test_existing_score(clean_redis):
         'notification_queue': 'our_queue'
     }).json())
 
-    log.init_logging("middleman")
-    logger = logging.getLogger('assemblyline.middleman.ingester')
+    submitter.try_run(volatile=True)
 
-    with mock.patch('al_core.middleman.run_submit.Middleman', mf):
-        al_core.middleman.run_submit.submitter(logger=logger, datastore=ds, volatile=True,
-                                               redis=clean_redis, persistent_redis=clean_redis)
-
-    mm = mf.instance
+    mm = submitter.middleman
     # No tasks should be left in the queue
     assert mm.unique_queue.pop() is None
-    # We should have received a notification about our task
+    # We should have received a notification about our task, since it was already 'done'
     assert mm.notification_queues['our_queue'].length() == 1
 
