acf112e58f6667ca7cc4413e5ca8a337ac40e35d
sgaron-cse <sgaron.cse@gmail.com>
2019-04-26 13:16:37 -0400

    setup.py files should not rely on requirements.txt files

--- setup.py
+++ setup.py
@@ -1,29 +1,17 @@
-import os
 
 from setuptools import setup, find_packages
 
 
-def read_requirements(path):
-    with open(os.path.join(os.path.dirname(__file__), path)) as req:
-        lines = req.read().split()
-
-    lines = [line.strip() for line in lines]
-    lines = [line.strip() for line in lines if len(line) > 0]
-    lines = [line.strip() for line in lines if line[0] != '#']
-
-    return lines
-
-
 setup(
     name="assemblyline-core",
-    version="4.0.0.dev3",
+    version="4.0.0.dev6",
     description="Assemblyline (v4) automated malware analysis framework - Core components.",
     long_description="This package provides the core components of Assemblyline v4 malware analysis framework. "
                      "(Alerter, Dispatcher, Expiry, Ingester, Metrics, Watcher, Workflow)",
     url="https://bitbucket.org/cse-assemblyline/alv4_core/",
     author="CCCS Assemblyline development team",
     author_email="assemblyline@cyber.gc.ca",
-    licence="MIT",
+    license="MIT",
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -37,8 +25,12 @@ setup(
     ],
     keywords="assemblyline malware gc canada cse-cst cse cst cyber cccs",
     packages=find_packages(),
-    install_requires=read_requirements('./requirements.txt'),
-    tests_requires=read_requirements('./test-requirements.txt'),
+    install_requires=[
+        'urllib3<1.25',
+        'python-baseconv',
+        'assemblyline',
+        'elastic-apm[flask]'
+    ],
     package_data={
         '': ["*schema.xml", "*managed-schema", "*solrconfig.xml", "*classification.yml", "*.magic"]
     }
