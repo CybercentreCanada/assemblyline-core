bb15ab2f985ff25447260406093f0f8e70bfcc0f
Adam Douglass <douglass.cse@gmail.com>
2019-05-27 13:04:07 -0400

    Add changes to pipeline in setup already done in other repos

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 
+import os
 from setuptools import setup, find_packages
+
+# For development and local builds use this version number, but for real builds replace it
+# with the tag found in the environment
+package_version = "4.0.0.dev0"
+for variable_name in ['BITBUCKET_TAG']:
+    package_version = os.environ.get(variable_name, package_version)
+    package_version = package_version.lstrip('v')
 
 
 setup(
     name="assemblyline-core",
-    version="4.0.0.dev9",
+    version=package_version,
     description="Assemblyline (v4) automated malware analysis framework - Core components.",
     long_description="This package provides the core components of Assemblyline v4 malware analysis framework. "
                      "(Alerter, Dispatcher, Expiry, Ingester, Metrics, Watcher, Workflow)",
