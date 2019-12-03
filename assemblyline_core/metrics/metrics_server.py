6fdcef26d64c2196c30e6cc3557096e9afe75514
sgaron-cse <sgaron.cse@gmail.com>
2019-12-03 12:05:48 -0500

    Try to fix dashboard hearbeats

--- assemblyline_core/metrics/metrics_server.py
+++ assemblyline_core/metrics/metrics_server.py
@@ -261,6 +261,8 @@ class HeartbeatManager(ServerBase):
         self.scheduler = BackgroundScheduler(daemon=True)
         self.hm = HeartbeatFormatter("heartbeat_manager", self.log, config=self.config)
 
+        self.counters_lock = Lock()
+        self.counters = {}
         self.rolling_window = {}
         self.window_ttl = {}
         self.ttl = self.config.core.metrics.export_interval * 2
@@ -293,20 +295,19 @@ class HeartbeatManager(ServerBase):
                 msg.pop('instance', None)
 
                 self.log.debug(f"Received {m_type.upper()} metrics message")
-                if not m_name or not m_type:
+                if not m_name or not m_type or not m_host:
                     # APM Transaction end
                     if self.apm_client:
                         self.apm_client.end_transaction('process_message', 'invalid_message')
 
                     continue
 
-                w_key = (m_name, m_type, m_host)
-                if w_key not in self.rolling_window or m_type in NON_AGGREGATED:
-                    self.rolling_window[w_key] = [Counter(msg)]
-                else:
-                    self.rolling_window[w_key].append(Counter(msg))
-
-                self.window_ttl[w_key] = time.time() + self.ttl
+                with self.counters_lock:
+                    c_key = (m_name, m_type, m_host)
+                    if c_key not in self.counters or m_type in NON_AGGREGATED:
+                        self.counters[c_key] = Counter(msg)
+                    else:
+                        self.counters[c_key] += Counter(msg)
 
                 # APM Transaction end
                 if self.apm_client:
@@ -327,8 +328,19 @@ class HeartbeatManager(ServerBase):
                     del self.window_ttl[k]
                     del self.rolling_window[k]
 
-            self.log.info("Copying counters ...")
-            window_copy = copy.deepcopy(self.rolling_window)
+            self.log.info("Saving current counters to rolling window ...")
+            with self.counters_lock:
+                counter_copy, self.counters = self.counters, {}
+
+            for w_key, counter in counter_copy.items():
+                _, m_type, _ = w_key
+                if w_key not in self.rolling_window or m_type in NON_AGGREGATED:
+                    self.rolling_window[w_key] = [counter]
+                else:
+                    self.rolling_window[w_key].append(counter)
+
+                self.rolling_window[w_key] = self.rolling_window[w_key][-self.window_size:]
+                self.window_ttl[w_key] = time.time() + self.ttl
 
             self.log.info("Compiling service list...")
             aggregated_counters = {}
@@ -347,22 +359,16 @@ class HeartbeatManager(ServerBase):
                 aggregated_counters[(service, 'service')] = Counter(data)
 
             self.log.info("Aggregating heartbeat data...")
-            for component_parts, counters_list in window_copy.items():
+            for component_parts, counters_list in self.rolling_window.items():
                 c_name, c_type, c_host = component_parts
 
                 # Expiring data outside of the window
-                if len(counters_list) > self.window_size:
-                    self.rolling_window[component_parts].pop(0)
-                    counters_list = counters_list[1:]
+                counters_list = counters_list[-self.window_size:]
 
                 key = (c_name, c_type)
                 if key not in aggregated_counters:
                     aggregated_counters[key] = Counter()
 
-                # if c_type in metrics.TIMED_METRICS:
-                #     aggregated_counters[key]['instances.t'] += 1
-                #     aggregated_counters[key]['instances.c'] += 1
-                # else:
                 aggregated_counters[key]['instances'] += 1
 
                 for c in counters_list:
