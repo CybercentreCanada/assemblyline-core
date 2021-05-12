14a51a41c447f80b9ab1fe14827a875133adbe24
Adam Douglass <adam.douglass@cyber.gc.ca>
2021-05-12 13:48:16 -0700

    improve test reliability

--- assemblyline_core/ingester/ingester.py
+++ assemblyline_core/ingester/ingester.py
@@ -142,7 +142,7 @@ class IngestTask(odm.Model):
 class Ingester(ThreadedCoreBase):
     def __init__(self, datastore=None, logger=None, classification=None, redis=None, persistent_redis=None,
                  metrics_name='ingester', config=None):
-        super().__init__('ingester', logger, redis=redis, redis_persist=persistent_redis, datastore=datastore,
+        super().__init__('assemblyline.ingester', logger, redis=redis, redis_persist=persistent_redis, datastore=datastore,
                          config=config)
 
         # Cache the user groups
