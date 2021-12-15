29ef166188113cfb69c82837f9d8db07a6b9efcf
Steve Garon <steve.garon@cyber.gc.ca>
2021-12-15 20:40:38 +0000

    Test and build on python 3.10

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     keywords="assemblyline automated malware analysis gc canada cse-cst cse cst cyber cccs",
     packages=find_packages(exclude=['deployment/*', 'test/*']),
