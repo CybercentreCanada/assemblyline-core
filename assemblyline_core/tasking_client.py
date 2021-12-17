ae726de615c39be78597ebbd064e10f618be5cbe
Steve Garon <steve.garon@cyber.gc.ca>
2021-12-17 14:30:58 +0000

    Remove safelisting from tasking client

--- assemblyline_core/tasking_client.py
+++ assemblyline_core/tasking_client.py
@@ -5,7 +5,6 @@ import time
 from typing import Any, Dict
 
 import elasticapm
-import yaml
 
 from assemblyline.common import forge, identify
 from assemblyline.common.constants import SERVICE_STATE_HASH, ServiceStatus
@@ -94,48 +93,6 @@ class TaskingClient:
         else:
             raise TaskingClientException("Uploaded file does not match expected file hash. "
                                          f"[{file_info['sha256']} != {expected_sha256}]")
-
-    # Safelist
-    def exists(self, qhash, **_):
-        return self.datastore.safelist.get_if_exists(qhash, as_obj=False)
-
-    def get_safelist_for_tags(self, tag_types, **_):
-        if tag_types:
-            tag_types = tag_types.split(',')
-
-        with forge.get_cachestore('system', config=self.config, datastore=self.datastore) as cache:
-            tag_safelist_yml = cache.get('tag_safelist_yml')
-            if tag_safelist_yml:
-                tag_safelist_data = yaml.safe_load(tag_safelist_yml)
-            else:
-                tag_safelist_data = forge.get_tag_safelist_data()
-
-        if tag_types:
-            output = {
-                'match': {k: v for k, v in tag_safelist_data.get('match', {}).items() if k in tag_types or tag_types == []},
-                'regex': {k: v for k, v in tag_safelist_data.get('regex', {}).items() if k in tag_types or tag_types == []},
-            }
-            for tag in tag_types:
-                for sl in self.datastore.safelist.stream_search(
-                        f"type:tag AND enabled:true AND tag.type:{tag}", as_obj=False):
-                    output['match'].setdefault(sl['tag']['type'], [])
-                    output['match'][sl['tag']['type']].append(sl['tag']['value'])
-
-        else:
-            output = tag_safelist_data
-            for sl in self.datastore.safelist.stream_search("type:tag AND enabled:true", as_obj=False):
-                output['match'].setdefault(sl['tag']['type'], [])
-                output['match'][sl['tag']['type']].append(sl['tag']['value'])
-
-        return output
-
-    def get_safelist_for_signatures(self, **_):
-        output = [
-            item['signature']['name']
-            for item in self.datastore.safelist.stream_search(
-                "type:signature AND enabled:true", fl="signature.name", as_obj=False)]
-
-        return output
 
     # Service
     def register_service(self, client_info, json, **_):
