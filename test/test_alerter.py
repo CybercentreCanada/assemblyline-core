9d54bc9c166370324ae46ee575513681c025ad0b
Steve Garon <steve.garon@cyber.gc.ca>
2023-09-20 19:56:48 +0000

    Fix alerter tests

--- test/test_alerter.py
+++ test/test_alerter.py
@@ -57,6 +57,7 @@ def test_create_single_alert(config, datastore):
     alerter = Alerter()
     # Swap our alerter onto a private queue so our test doesn't get intercepted
     alerter.alert_queue = alert_queue = NamedQueue(uuid.uuid4().hex, persistent_redis)
+    alert_queue.delete()
 
     # Get a random submission
     submission = random.choice(all_submissions)
@@ -90,6 +91,7 @@ def test_update_single_alert(config, datastore, delete_original=False):
     alerter = Alerter()
     # Swap our alerter onto a private queue so our test doesn't get intercepted
     alerter.alert_queue = alert_queue = NamedQueue(uuid.uuid4().hex, persistent_redis)
+    alert_queue.delete()
 
     # Get a random submission
     submission = random.choice(all_submissions)
