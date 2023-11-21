db21a689854119fda6566728bf93e3632851f5be
Steve Garon <steve.garon@cyber.gc.ca>
2023-11-21 14:59:16 +0000

    Make sure drop_chance function doesn't go below 0

--- assemblyline_core/ingester/constants.py
+++ assemblyline_core/ingester/constants.py
@@ -5,4 +5,4 @@ INGEST_QUEUE_NAME = 'm-ingest'
 
 
 def drop_chance(length, maximum):
-    return tanh(float(length - maximum) / maximum * 2.0)
+    return max(0, tanh(float(length - maximum) / maximum * 2.0))
