c475534c934129e7bb81ee3d6335da1763b5c6fd
cccs-rs <ryan.samaroo@cyber.gc.ca>
2024-02-04 06:25:23 +0000

    Shift hash item merging into respective clients

--- assemblyline_core/safelist_client.py
+++ assemblyline_core/safelist_client.py
@@ -3,7 +3,14 @@ import logging
 import yaml
 
 from assemblyline.common import forge
+from assemblyline.common.isotime import now_as_iso
 from assemblyline.datastore.helper import AssemblylineDatastore
+
+CLASSIFICATION = forge.get_classification()
+
+
+class InvalidSafehash(Exception):
+    pass
 
 
 # Tasking class
@@ -58,3 +65,64 @@ class SafelistClient:
                 "type:signature AND enabled:true", fl="signature.name", as_obj=False)]
 
         return output
+
+    @staticmethod
+    def _merge_hashes(new, old):
+        try:
+            # Check if hash types match
+            if new['type'] != old['type']:
+                raise InvalidSafehash(f"Safe hash type mismatch: {new['type']} != {old['type']}")
+
+            # Use the new classification but we will recompute it later anyway
+            old['classification'] = new['classification']
+
+            # Update updated time
+            old['updated'] = new.get('updated', now_as_iso())
+
+            # Update hashes
+            old['hashes'].update({k: v for k, v in new['hashes'].items() if v})
+
+            # Update type specific info
+            if old['type'] == 'file':
+                old.setdefault('file', {})
+                new_names = new.get('file', {}).pop('name', [])
+                if 'name' in old['file']:
+                    for name in new_names:
+                        if name not in old['file']['name']:
+                            old['file']['name'].append(name)
+                elif new_names:
+                    old['file']['name'] = new_names
+                old['file'].update({k: v for k, v in new.get('file', {}).items() if v})
+            elif old['type'] == 'tag':
+                old['tag'] = new['tag']
+
+            # Merge sources
+            src_map = {x['name']: x for x in new['sources']}
+            if not src_map:
+                raise InvalidSafehash("No valid source found")
+
+            old_src_map = {x['name']: x for x in old['sources']}
+            for name, src in src_map.items():
+                if name not in old_src_map:
+                    old_src_map[name] = src
+                else:
+                    old_src = old_src_map[name]
+                    if old_src['type'] != src['type']:
+                        raise InvalidSafehash(f"Source {name} has a type conflict: {old_src['type']} != {src['type']}")
+
+                    for reason in src['reason']:
+                        if reason not in old_src['reason']:
+                            old_src['reason'].append(reason)
+                    old_src['classification'] = src.get('classification', old_src['classification'])
+            old['sources'] = list(old_src_map.values())
+
+            # Calculate the new classification
+            for src in old['sources']:
+                old['classification'] = CLASSIFICATION.max_classification(
+                    old['classification'], src.get('classification', None))
+
+            # Set the expiry
+            old['expiry_ts'] = new.get('expiry_ts', None)
+            return old
+        except Exception as e:
+            raise InvalidSafehash(f"Invalid data provided: {str(e)}")
