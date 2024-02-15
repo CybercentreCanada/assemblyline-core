62bb1ce8e474ee71b9db0df100120d42c454664e
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-02-15 18:10:10 +0000

    Shift hash list add_update logic to respective clients with testing

--- assemblyline_core/badlist_client.py
+++ assemblyline_core/badlist_client.py
@@ -5,6 +5,9 @@ from assemblyline.common import forge
 from assemblyline.common.chunk import chunk
 from assemblyline.common.isotime import now_as_iso
 from assemblyline.datastore.helper import AssemblylineDatastore
+from assemblyline.odm.models.user import ROLES
+from assemblyline.remote.datatypes.lock import Lock
+
 
 CHUNK_SIZE = 1000
 CLASSIFICATION = forge.get_classification()
@@ -22,7 +25,115 @@ class BadlistClient:
         self.config = config or forge.CachedObject(forge.get_config)
         self.datastore = datastore or forge.get_datastore(self.config)
 
-    # Badlist
+    def _preprocess_object(self, data: dict) -> str:
+        # Set defaults
+        data.setdefault('classification', CLASSIFICATION.UNRESTRICTED)
+        data.setdefault('hashes', {})
+        data.setdefault('expiry_ts', None)
+        if data['type'] == 'tag':
+            # Remove file related fields
+            data.pop('file', None)
+            data.pop('hashes', None)
+
+            tag_data = data.get('tag', None)
+            if tag_data is None or 'type' not in tag_data or 'value' not in tag_data:
+                raise ValueError("Tag data not found")
+
+            hashed_value = f"{tag_data['type']}: {tag_data['value']}".encode('utf8')
+            data['hashes'] = {
+                'md5': hashlib.md5(hashed_value).hexdigest(),
+                'sha1': hashlib.sha1(hashed_value).hexdigest(),
+                'sha256': hashlib.sha256(hashed_value).hexdigest()
+            }
+
+        elif data['type'] == 'file':
+            data.pop('tag', None)
+            data.setdefault('file', {})
+
+        # Ensure expiry_ts is set on tag-related items
+        dtl = data.pop('dtl', None) or self.config.core.expiry.badlisted_tag_dtl * 24 * 3600
+        if dtl:
+            data['expiry_ts'] = now_as_iso(dtl)
+
+        # Set last updated
+        data['added'] = data['updated'] = now_as_iso()
+
+        # Find the best hash to use for the key
+        for hash_key in ['sha256', 'sha1', 'md5']:
+            qhash = data['hashes'].get(hash_key, None)
+            if qhash:
+                break
+
+        # Validate hash length
+        if not qhash:
+            raise ValueError("No valid hash found")
+
+        return qhash
+
+    def add_update(self, badlist_object: dict, user: dict = None):
+        qhash = self._preprocess_object(badlist_object)
+
+        # Validate sources
+        src_map = {}
+        for src in badlist_object['sources']:
+            if user:
+                if src['type'] == 'user':
+                    if src['name'] != user['uname']:
+                        raise ValueError(f"You cannot add a source for another user. {src['name']} != {user['uname']}")
+                else:
+                    if ROLES.signature_import not in user['roles']:
+                        raise PermissionError("You do not have sufficient priviledges to add an external source.")
+
+            # Find the highest classification of all sources
+            badlist_object['classification'] = CLASSIFICATION.max_classification(
+                badlist_object['classification'], src.get('classification', None))
+
+            src_map[src['name']] = src
+
+        with Lock(f'add_or_update-badlist-{qhash}', 30):
+            old = self.datastore.badlist.get_if_exists(qhash, as_obj=False)
+            if old:
+                # Save data to the DB
+                self.datastore.badlist.save(qhash, BadlistClient._merge_hashes(badlist_object, old))
+                return qhash, "update"
+            else:
+                try:
+                    badlist_object['sources'] = list(src_map.values())
+                    self.datastore.badlist.save(qhash, badlist_object)
+                    return qhash, "add"
+                except Exception as e:
+                    return ValueError(f"Invalid data provided: {str(e)}")
+
+    def add_update_many(self, list_of_badlist_objects: list):
+        if not isinstance(list_of_badlist_objects, list):
+            raise ValueError("Could not get the list of hashes")
+
+        new_data = {}
+        for badlist_object in list_of_badlist_objects:
+            qhash = self._preprocess_object(badlist_object)
+            new_data[qhash] = badlist_object
+
+        # Get already existing hashes
+        old_data = self.datastore.badlist.multiget(list(new_data.keys()), as_dictionary=True, as_obj=False,
+                                                   error_on_missing=False)
+
+        # Test signature names
+        plan = self.datastore.badlist.get_bulk_plan()
+        for key, val in new_data.items():
+            # Use maximum classification
+            old_val = old_data.get(key, {'classification': CLASSIFICATION.UNRESTRICTED, 'attribution': {},
+                                         'hashes': {}, 'sources': [], 'type': val['type']})
+
+            # Add upsert operation
+            plan.add_upsert_operation(key, BadlistClient._merge_hashes(val, old_val))
+
+        if not plan.empty:
+            # Execute plan
+            res = self.datastore.badlist.bulk(plan)
+            return {"success": len(res['items']), "errors": res['errors']}
+
+        return {"success": 0, "errors": []}
+
     def exists(self, qhash):
         return self.datastore.badlist.get_if_exists(qhash, as_obj=False)
 
--- assemblyline_core/safelist_client.py
+++ assemblyline_core/safelist_client.py
@@ -1,10 +1,12 @@
+import hashlib
 import logging
-
 import yaml
 
 from assemblyline.common import forge
 from assemblyline.common.isotime import now_as_iso
 from assemblyline.datastore.helper import AssemblylineDatastore
+from assemblyline.odm.models.user import ROLES
+from assemblyline.remote.datatypes.lock import Lock
 
 CLASSIFICATION = forge.get_classification()
 
@@ -22,7 +24,134 @@ class SafelistClient:
         self.config = config or forge.CachedObject(forge.get_config)
         self.datastore = datastore or forge.get_datastore(self.config)
 
-    # Safelist
+    def _preprocess_object(self, data: dict):
+        # Set defaults
+        data.setdefault('classification', CLASSIFICATION.UNRESTRICTED)
+        data.setdefault('hashes', {})
+        data.setdefault('expiry_ts', None)
+        if data['type'] == 'tag':
+            # Remove file related fields
+            data.pop('file', None)
+            data.pop('hashes', None)
+            data.pop('signature', None)
+
+            tag_data = data.get('tag', None)
+            if tag_data is None or 'type' not in tag_data or 'value' not in tag_data:
+                raise ValueError("Tag data not found")
+
+            hashed_value = f"{tag_data['type']}: {tag_data['value']}".encode('utf8')
+            data['hashes'] = {
+                'md5': hashlib.md5(hashed_value).hexdigest(),
+                'sha1': hashlib.sha1(hashed_value).hexdigest(),
+                'sha256': hashlib.sha256(hashed_value).hexdigest()
+            }
+
+        elif data['type'] == 'signature':
+            # Remove file related fields
+            data.pop('file', None)
+            data.pop('hashes', None)
+            data.pop('tag', None)
+
+            sig_data = data.get('signature', None)
+            if sig_data is None or 'name' not in sig_data:
+                raise ValueError("Signature data not found")
+
+            hashed_value = f"signature: {sig_data['name']}".encode('utf8')
+            data['hashes'] = {
+                'md5': hashlib.md5(hashed_value).hexdigest(),
+                'sha1': hashlib.sha1(hashed_value).hexdigest(),
+                'sha256': hashlib.sha256(hashed_value).hexdigest()
+            }
+
+        elif data['type'] == 'file':
+            data.pop('signature', None)
+            data.pop('tag', None)
+            data.setdefault('file', {})
+
+        # Ensure expiry_ts is set on tag-related items
+        dtl = data.pop('dtl', None) or self.config.core.expiry.safelisted_tag_dtl * 24 * 3600
+        if dtl:
+            data['expiry_ts'] = now_as_iso(dtl)
+
+        # Set last updated
+        data['added'] = data['updated'] = now_as_iso()
+
+        # Find the best hash to use for the key
+        for hash_key in ['sha256', 'sha1', 'md5']:
+            qhash = data['hashes'].get(hash_key, None)
+            if qhash:
+                break
+
+        # Validate hash length
+        if not qhash:
+            raise ValueError("No valid hash found")
+
+        return qhash
+
+    def add_update(self, safelist_object: dict, user: dict = None):
+        qhash = self._preprocess_object(safelist_object)
+
+        # Validate sources
+        src_map = {}
+        for src in safelist_object['sources']:
+            if user:
+                if src['type'] == 'user':
+                    if src['name'] != user['uname']:
+                        raise ValueError(f"You cannot add a source for another user. {src['name']} != {user['uname']}")
+                else:
+                    if ROLES.signature_import not in user['roles']:
+                        raise PermissionError("You do not have sufficient priviledges to add an external source.")
+
+            # Find the highest classification of all sources
+            safelist_object['classification'] = CLASSIFICATION.max_classification(
+                safelist_object['classification'], src.get('classification', None))
+
+            src_map[src['name']] = src
+
+        with Lock(f'add_or_update-safelist-{qhash}', 30):
+            old = self.datastore.safelist.get_if_exists(qhash, as_obj=False)
+            if old:
+                # Save data to the DB
+                self.datastore.safelist.save(qhash, SafelistClient._merge_hashes(safelist_object, old))
+                return qhash, "update"
+            else:
+                try:
+                    safelist_object['sources'] = list(src_map.values())
+                    self.datastore.safelist.save(qhash, safelist_object)
+                    return qhash, "add"
+                except Exception as e:
+                    return ValueError(f"Invalid data provided: {str(e)}")
+
+    def add_update_many(self, list_of_safelist_objects: list):
+        if not isinstance(list_of_safelist_objects, list):
+            raise ValueError("Could not get the list of hashes")
+
+        new_data = {}
+        for safelist_object in list_of_safelist_objects:
+            qhash = self._preprocess_object(safelist_object)
+            new_data[qhash] = safelist_object
+
+        # Get already existing hashes
+        old_data = self.datastore.safelist.multiget(list(new_data.keys()), as_dictionary=True, as_obj=False,
+                                                    error_on_missing=False)
+
+        # Test signature names
+        plan = self.datastore.safelist.get_bulk_plan()
+        for key, val in new_data.items():
+            # Use maximum classification
+            old_val = old_data.get(key, {'classification': CLASSIFICATION.UNRESTRICTED,
+                                         'hashes': {}, 'sources': [], 'type': val['type']})
+
+            # Add upsert operation
+            plan.add_upsert_operation(key, SafelistClient._merge_hashes(val, old_val))
+
+        if not plan.empty:
+            # Execute plan
+            res = self.datastore.safelist.bulk(plan)
+            return {"success": len(res['items']), "errors": res['errors']}
+
+        return {"success": 0, "errors": []}
+
     def exists(self, qhash):
         return self.datastore.safelist.get_if_exists(qhash, as_obj=False)
 
