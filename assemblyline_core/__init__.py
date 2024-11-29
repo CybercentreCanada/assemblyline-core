493c4ca337105c33e097824bf54f6e0c91aa2b14
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-11-29 13:02:27 -0500

    Update assemblyline_core/__init__.py
    Co-authored-by: gdesmar <75089569+gdesmar@users.noreply.github.com>

--- assemblyline_core/__init__.py
+++ assemblyline_core/__init__.py
@@ -6,5 +6,5 @@ def normalize_hashlist_item(tag_type: str, tag_value: str) -> str:
         tag_value = tag_value.lower()
     elif tag_type.endswith('.uri'):
         hostname = tag_value.split('//', 1)[1].split('/', 1)[0]
-        tag_value = tag_value.replace(hostname, hostname.lower())
+        tag_value = tag_value.replace(hostname, hostname.lower(), 1)
     return tag_value
